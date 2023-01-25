4b7c540ca0438ea542fbc48f71718c351c18696b
ariancy <37824506+yyechan0602@users.noreply.github.com>
2023-01-26 02:30:40 +0900

    Initial commit

---
+++ Code/src/Math/n2839.java
@@ -0,0 +1,27 @@
+package Math;
+import java.util.Scanner;
+
+public class n2839 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int suger = sc.nextInt();
+
+        System.out.println(bag(suger));
+    }
+    public static int bag(int suger){
+        int result = 0;
+
+        while (suger >2) {
+            if(suger%5 == 0) {
+                result += suger/5;
+                suger = 0;
+                break;
+            }
+            suger = suger - 3;
+            result += 1;
+        }
+        if (suger != 0) {return -1;}
+        return result;
+    }
+}
